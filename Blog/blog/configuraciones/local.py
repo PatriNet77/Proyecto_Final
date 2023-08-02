bdba97f05b700f7eb8ff1adf792ac81e7641493e
PatriNet77 <131683643+PatriNet77@users.noreply.github.com>
2023-08-02 12:51:08 -0300

    blog
    Contiene configuraciones y el settings

---
+++ Blog/blog/configuraciones/local.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Soy_Burrodb',
+        'USER': 'root',
+        'PASSWORD': '@100xCienTo571318',
+        'HOST': 'localhost',
+        'PORT': 3306,
+    }
+}
---
+++ Blog/blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from apps.posts.models import Post
+
+def index(request):
+    post_recientes = Post.objects.all().order_by('-fecha')[0:3]
+    contexto = {'post_recientes':post_recientes}
+    return render(request, 'index.html', contexto)
